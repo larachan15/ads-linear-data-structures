1046a55121db5d338f44bfbb91ade4e8f23ce0fe
Dionisia Montanez <dmontanez15@gmail.com>
2020-06-16 18:40:22 -0700

    Fix infinite loop issue in test.

--- src/data_structures/dll_queue.js
+++ src/data_structures/dll_queue.js
@@ -62,7 +62,7 @@ class DLLQueue {
    * @param {forEachCallback} callback Function to invoke
    */
   forEach(callback) {
-    this.storage.forEach(callback, this);
+    return this.storage.forEach(callback, this);
   }
 }
 
